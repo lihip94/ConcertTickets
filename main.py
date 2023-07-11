adb672c2d9433a21af9ad50ff57c6e6ef6d431bd
lihip <lihipana@gmail.com>
2023-07-11 10:06:37 +0300

    add Alef font to have the ability to write in hebrew in the pdf

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+from reportlab.lib.enums import TA_RIGHT
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+
+
+pdfmetrics.registerFont(TTFont("Alef", "Alef-Regular.ttf"))
+styles = getSampleStyleSheet()
+styles.add(ParagraphStyle(name='Right', alignment=TA_RIGHT, fontName='Alef', fontSize=10))
 
 
 def generate_pdf(person, concert_date):
-    filename = f"{person}-{concert_date}.pdf"
+    filename = f"output\\{person}-{concert_date}.pdf"
     # Create a canvas with the specified output file name and page size
     c = canvas.Canvas(filename=filename, pagesize=letter)
     # Set font properties
-    c.setFont("Helvetica", 12)
-    c.drawString(50, 700, f"Concert Date: {concert_date}")
-    c.drawString(200, 700, f"Person name: {person}")
+    c.setFont("Alef", 12)
+    c.drawString(50, 700, f"תאריך הקונצרט: {concert_date}"[::-1])
+    c.drawString(200, 700, f"שם: {person}"[::-1])
     return c
 
 
@@ -22,7 +31,7 @@ def add_info_to_pdf(data):
             y_position = 670
             for seat in seats:
                 row, chair = seat
-                c.drawString(70, y_position, f"Row: {row} | Chair: {chair}")
+                c.drawString(70, y_position, f"שורה: {row} | כיסא: {chair}"[::-1])
                 y_position -= 20
             y_position -= 20
             # Save the PDF
